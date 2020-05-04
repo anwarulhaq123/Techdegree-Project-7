2a349f68cd0f69433f3b6ae026c0e67655eab4d6
Muhammad Anwar Ul Haq <48963489+anwarulhaq123@users.noreply.github.com>
2020-05-04 07:43:47 +0100

    Update PhotoContainer.js

--- src/components/PhotoContainer.js
+++ src/components/PhotoContainer.js
@@ -2,23 +2,23 @@
 
 import React, {Component} from "react";
 
-//Components for PhotoContainer
+//import components
 import Photo from "./Photo";
 import NotFound from "./NotFound";
 
 
-//Class for PhotContainer
+//Photo Container Class handling gallery
 
 export default class PhotoContainer extends Component {
     //LifeCycle component to perform the Search 
     componentDidMount() {
-        this.props.doSearch(this.props.routeMatch);
+        this.props.doSearch(this.props.matchData.params.query);
     }
-    //Update the Page every times it Loads
+    //Update the Page every times it Loads.
     componentDidUpdate(prevProps) {
         //console.log(prevProps);
-        if(this.props.routeMatch.params.query !== prevProps.routeMatch.params.query){
-            this.props.doSearch(this.props.routeMatch.params.query);
+        if(this.props.matchData.params.query !== prevProps.matchData.params.query){
+            this.props.doSearch(this.props.matchData.params.query);
         }
     }
 
@@ -28,28 +28,28 @@ export default class PhotoContainer extends Component {
         const mainImages = this.props.data;
         let images;
 
-
-   
-        if(mainImages.length > 0){
-            images = mainImages.map( img => <Photo url={`https://farm${img.farm}.staticflickr.com/${img.server}/${img.id}_${img.secret}.jpg`} key={img.id} />)
+        // when there are results append theme to a PhotoContainer.
+       if(mainImages.length > 0){
+       images = mainImages.map( img => <Photo url={`https://farm${img.farm}.staticflickr.com/${img.server}/${img.id}_${img.secret}.jpg`} key={img.id} />)
         }else {
           //if there are no results, show Not found component.
         images = <NotFound />;
           }
 
- 
-        
+       
 
 
         return(
             <div className="photo-container">
-                <h2>Results for {this.props.routeMatch.params.query}</h2>
+                <h2>Results for {this.props.matchData.params.query}</h2>
                 <ul> 
                     {images}
+
                 </ul>
             </div>
 
         );
     }
 }
+
 

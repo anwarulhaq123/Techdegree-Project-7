bac21315a779687480bf2ff70ec15e3f5eadeeac
Muhammad Anwar Ul Haq <48963489+anwarulhaq123@users.noreply.github.com>
2020-05-05 18:52:07 +0100

    Create LoadingImg.js

---
+++ src/LoadingImg.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+//Function LoadingImg
+const LoadingImg = () => (
+    <li>
+        <p> loading...</p>
+    </li>
+);
+// Export the Function 
+export default LoadingImg;

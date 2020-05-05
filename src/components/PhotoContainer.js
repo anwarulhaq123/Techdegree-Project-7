ea7135f44d36052dbb9e815c251006b6a50f0c7f
Muhammad Anwar Ul Haq <48963489+anwarulhaq123@users.noreply.github.com>
2020-05-05 19:28:55 +0100

    Update PhotoContainer.js

--- src/components/PhotoContainer.js
+++ src/components/PhotoContainer.js
@@ -1,10 +1,10 @@
 
-
 import React, {Component} from "react";
 
 //import components
 import Photo from "./Photo";
 import NotFound from "./NotFound";
+import LoadingImg from "./LoadingImg";
 
 
 //Photo Container Class handling gallery
@@ -16,11 +16,13 @@ export default class PhotoContainer extends Component {
     }
     //Update the Page every times it Loads.
     componentDidUpdate(prevProps) {
-        //console.log(prevProps);
+   
         if(this.props.matchData.params.query !== prevProps.matchData.params.query){
             this.props.doSearch(this.props.matchData.params.query);
         }
     }
+    
+
 
   
     render() {
@@ -28,13 +30,20 @@ export default class PhotoContainer extends Component {
         const mainImages = this.props.data;
         let images;
 
+        if(this.props.page){
+            images = <LoadingImg />
+        }
+
+     
+
         // when there are results append theme to a PhotoContainer.
        if(mainImages.length > 0){
        images = mainImages.map( img => <Photo url={`https://farm${img.farm}.staticflickr.com/${img.server}/${img.id}_${img.secret}.jpg`} key={img.id} />)
-        }else {
-          //if there are no results, show Not found component.
-        images = <NotFound />;
-          }
+        }
+
+        if(!this.props.page && mainImages.length === 0 ){
+            images = <NotFound />
+        }
 
        
 

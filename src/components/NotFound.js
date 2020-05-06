626f2d668d37567a39557c4298cc041a741df162
Muhammad Anwar Ul Haq <48963489+anwarulhaq123@users.noreply.github.com>
2020-05-06 02:12:13 +0100

    Update NotFound.js

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -5,7 +5,7 @@ const NotFound = () => (
     <li className="not-found">
         
             <h3>No Results Found</h3>
-            <p>You search did not return any results. Please try again.</p>
+            <p>Your search did not return any results. Please try again.</p>
     </li>
 );
 //Export the NotFounnd

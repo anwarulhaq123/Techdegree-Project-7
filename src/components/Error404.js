bc63e28d45f6681a6bad4eeb7f3fbc400ac117d6
Muhammad Anwar Ul Haq <48963489+anwarulhaq123@users.noreply.github.com>
2020-05-06 01:21:03 +0100

    Add files via upload

---
+++ src/components/Error404.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+// Create a Error function 
+const Error404 = ( ) => (
+<div >
+<h1 className ='message'>Sorry this page not Exist</h1>
+<p className = 'not-exist'> Error 404</p>
+</div>
+);
+
+// Export default function 
+export default Error404;
